9b89a683b907981dc7b79db7fda6302e477797a1
Mathew Vaughan <mathew.vaughan@cazoo.co.uk>
2023-02-12 17:45:11 +0000

    move test data in own file

---
+++ src/data.py
@@ -0,0 +1,9 @@
+
+data = [
+        {
+            "id": "0",
+            "title_number": "MYBKZ10625",
+            "title_class": "Freehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus ru"
+        },
+    ]
--- src/test_app.py
+++ src/test_app.py
@@ -1,15 +1,7 @@
 from fastapi.testclient import TestClient
 
 from .main import get_app
-
-data = [
-        {
-            "id": "0",
-            "title_number": "MYBKZ10625",
-            "title_class": "Freehold",
-            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus ru"
-        },
-    ]
+from .data import data
 
 client = TestClient(get_app(
     data
