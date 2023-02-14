5a24648fcc2722d4af32178aafe226abb1c733ab
Mathew Vaughan <mathew.vaughan@cazoo.co.uk>
2023-02-14 11:58:25 +0100

    add test for ability to specify page

--- src/test_list_endpoint.py
+++ src/test_list_endpoint.py
@@ -22,3 +22,11 @@ def test_list_endpoint_returns_paginated_response_with_custom_page_size():
     assert response.json()["size"] == 5
     assert response.json()["total"] == len(data)
     assert response.json()["items"] == data[:5]
+
+def test_list_endpoint_returns_paginated_response_with_custom_page():
+    response = client.get("/api/titles?page=2&size=5")
+    assert response.status_code == 200
+    assert response.json()["page"] == 2
+    assert response.json()["size"] == 5
+    assert response.json()["total"] == len(data)
+    assert response.json()["items"] == data[5:9]
