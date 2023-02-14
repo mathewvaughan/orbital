5a76d30ab9f01042e674d9f32644681de20d3415
Mathew Vaughan <mathew.vaughan@cazoo.co.uk>
2023-02-14 12:34:38 +0100

    rename test fixture file so its clearly for tests

---
+++ src/test_data.py
@@ -0,0 +1,33 @@
+
+data = [
+        {
+            "id": "0",
+            "title_number": "MYBKZ10625",
+            "title_class": "Freehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus ru"
+        },
+        {
+            "id": "1",
+            "title_number": "GP51",
+            "title_class": "Leasehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequa"
+        },
+        {
+            "id": "2",
+            "title_number": "OQKH03830835",
+            "title_class": "Freehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus rutrum eu. Curabitur euismod dignissim turpis, vitae tincidunt leo condimentum non. Donec id dui in libero feugiat aliquet nec eu nulla. Proin eu enim dictum, viverra lacus vitae, porta arcu. Nam ullamcorper, odio in volutpat fermentum, odi"
+        },
+        {
+            "id": "3",
+            "title_number": "RR9963",
+            "title_class": "Leasehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus rutrum eu. Curabitur euismod dignissim turpis, vitae tincidunt leo condimentum non. Donec id dui in libero feugiat aliquet nec eu nulla. Proin eu enim dictum, viverra lacus vitae, porta arcu. Nam ullamcorper, odio in volutpat fermentum, odio lectus grav"
+        },
+        {
+            "id": "4",
+            "title_number": "ZULV94",
+            "title_class": "Freehold",
+            "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis lectus velit, ac mollis lorem fringilla ac. In consequat molestie dui, et pellentesque nisl convallis at. Curabitur dictum lacinia justo, pulvinar pharetra purus rutrum eu. Curabitur euismod dignissim turpis, vitae tincidunt leo condimentum non. Donec id dui in libero feugiat aliquet nec eu nulla. Proin eu enim dictum, viverra lacus vitae, porta arcu. Nam ullamcorper, odio in volutpat fermentum, odio lectus gravida nunc, eget imperdiet nibh nisi at elit. Suspendisse dapibu"
+        },
+    ]
